c308afd52e8826057443d6907a1c33272739bc82
Željko Plesac <zeljko.plesac@infinum.hr>
2015-01-06 12:37:27 +0100

    komentiranje metoda

--- app/src/main/java/co/infinum/connectionutils/presenters/ConnectionPresenter.java
+++ app/src/main/java/co/infinum/connectionutils/presenters/ConnectionPresenter.java
@@ -4,10 +4,21 @@ import android.content.Context;
 
 /**
  * Created by zeljkoplesac on 06/01/15.
+
+    Default ConnectionPresenter, which must be extended by our application BasePresenter
+
  */
 public interface ConnectionPresenter {
 
+    /**
+     * Activity or fragment should register for network updates on its onStart() method.
+     * @param context Activity context
+     */
     public void registerForNetworkUpdates(Context context);
 
+    /**
+     * Activity or fragment should unregister for network updates on its onStop() method.
+     * @param context Activity context
+     */
     public void unregisterFromNetworkUpdates(Context context);
 }
