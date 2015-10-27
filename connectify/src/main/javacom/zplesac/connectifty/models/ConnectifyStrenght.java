32d76c1041a35c351c4caae41d726db1d453d6df
zplesac <zeljko.plesac@infinum.hr>
2015-10-27 08:56:50 +0100

    added mininum signal strength to configuration

---
+++ connectify/src/main/javacom/zplesac/connectifty/models/ConnectifyStrenght.java
@@ -0,0 +1,15 @@
+package com.zplesac.connectifty.models;
+
+/**
+ * Created by Željko Plesac on 25/10/15.
+ */
+public enum ConnectifyStrenght {
+
+    UNDEFINED,
+
+    POOR,
+
+    GOOD,
+
+    EXCELLENT,
+}
