967f25e6d09c3aac1c000fab4a922cdb1021c82e
Franko Leon Tokalić <franko.tokalic@infinum.hr>
2020-03-18 18:14:02 +0100

    Suppress "unused" warnings

--- connectionbuddy/src/main/java/com/zplesac/connectionbuddy/ConnectionBuddy.java
+++ connectionbuddy/src/main/java/com/zplesac/connectionbuddy/ConnectionBuddy.java
@@ -45,7 +45,7 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 /**
  * Created by Željko Plesac on 06/10/14.
  */
-@SuppressWarnings("WeakerAccess")
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class ConnectionBuddy {
 
     private static final String HEADER_KEY_USER_AGENT = "User-Agent";
